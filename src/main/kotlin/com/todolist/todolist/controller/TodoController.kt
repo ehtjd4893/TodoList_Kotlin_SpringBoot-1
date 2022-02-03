32a87f262c213191824c6cd97aa5c552a2102a9b
ehtjd4893 <ehtjd4893@nate.com>
2022-02-03 19:38:07 +0900

    Settings And some

---
+++ src/main/kotlin/com/todolist/todolist/controller/TodoController.kt
@@ -0,0 +1,18 @@
+package com.todolist.todolist.controller
+
+import com.todolist.todolist.service.TodoService
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/todo")
+class TodoController(
+        private val todoService: TodoService
+) {
+    @GetMapping
+
+    @PostMapping
+
+    @PutMapping
+
+    @DeleteMapping
+}
